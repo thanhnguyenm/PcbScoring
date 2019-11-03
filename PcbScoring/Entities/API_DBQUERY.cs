ae298bbfd8a0b9224cbd1377807ba08ee236ecb6
Nguyen Minh Thanh <thanh.nguyen.59@fecredit.com.vn>
2019-11-04 00:16:58 +0700

    add new  models

---
+++ PcbScoring/Entities/API_DBQUERY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PcbScoring.Entities
+{
+    public class API_DBQUERY
+    {
+        public int ID { get; set; }
+        public string DBQUERY { get; set; }
+        public int PARAMID { get; set; }
+    }
+}
---
+++ PcbScoring/Entities/API_XPATHQUERY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PcbScoring.Entities
+{
+    public class API_XPATHQUERY
+    {
+        public int ID { get; set; }
+        public string XPATH { get; set; }
+        public int PARAMID { get; set; }
+    }
+}
